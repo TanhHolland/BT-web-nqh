3b278c5ccdf5164c9cfc5584ad2ee1b7bfc190fa
Your Name01~ <you@example.com>
2024-02-29 23:55:21 +0700

    check user

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,27 +1,37 @@
-// var checkAccount = (User) => {
-//     let username = document.forms['form-login']['username']
-//     let password = document.forms['form-login']['password']
-//     if (username.value == User.username) {
-//         if (password.value == User.password) {
-//             return true
-//         }
-//     }
-//     return false
-// }
-// var API = 'http://localhost:3000/User'
-// fetch(API) 
-//     .then((response) => response.json())
-//     .then((listUser) => {
-//         let username = document.forms['form-login']['username']
-//         isSuccess = false;
-//         for (let user in listUser) {
-//             if (checkAccount(user)) {
-//                 isSuccess = true
-//             }
-//             else {
-//                 username.insertAdjacentText("afterend", 'Tai khoan khong dung');
-//             }
-//         }
-//     })
-let username = document.getElementsByClassName('username-login')
-console.log(username.value)
+var API = "http://localhost:3000/User";
+var validateForm = () => {
+    let username = document.forms["form-login"]["username"].value;
+    let password = document.forms["form-login"]["password"].value;
+    let nameError = document.getElementById("username__error");
+    let passwordError = document.getElementById("password__error");
+    let accountError = document.getElementById("account__error");
+    // Khi submit thì dòng thông báo biến mất
+    nameError.innerHTML = "";
+    passwordError.innerHTML = "";
+    accountError.innerHTML = "";
+
+    if (username === "") {
+        nameError.innerHTML = "Vui lòng điền tài khoản";
+        return false;
+    }
+    if (password === "") {
+        passwordError.innerHTML = "Vui lòng điền mật khẩu";
+        return false;
+    }
+    // GET API
+    fetch(API)
+        .then(response => response.json())
+        .then(listUser => {
+            isValid = listUser.some((user) => {
+                return user.username === username && user.password === password;
+            });
+            console.log(isValid)
+            if (!isValid) {
+                accountError.innerHTML = "Tài khoản hoặc mật khẩu không hợp lệ";
+            }
+            else {
+                document.getElementById('form-fill').submit();
+            }
+        })
+    return false;
+};
